c6d26b778ec64e2166414e0e38d8c5e1b3c891dd
Joy_Admin <joyjedid@gmail.com>
2016-12-25 01:05:51 +0100

    db changes

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         //$this->call('CommandeTableSeeder');
         //$this->call('UsersTableSeeder');
         //$this->call('CamionTableSeeder');
-        //$this->call('RoleTableSeeder');
+        $this->call('RoleTableSeeder');
         $this->call('PermissionTableSeeder');
     }
 }
